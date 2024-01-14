56558cc7713970c1581468049ccd21bebb4074ec
Rafael Cangahuala <78826890+rcpc265@users.noreply.github.com>
2024-01-14 18:45:08 -0500

    Add a markdown editor

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,14 +1,21 @@
 "use client";
-import { Button, Heading, TextArea, TextField } from "@radix-ui/themes";
+import { Button, Heading, TextField } from "@radix-ui/themes";
+import "easymde/dist/easymde.min.css";
+import dynamic from "next/dynamic";
+
+const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
+  ssr: false,
+});
 
 const NewIssuePage = () => {
   return (
     <div className="max-w-xl space-y-3 mx-auto">
-      <Heading>Submit a new issue</Heading>
+      <Heading> New Issue </Heading>
       <TextField.Root>
         <TextField.Input placeholder="Title" />
       </TextField.Root>
-      <TextArea placeholder="Description" /> <Button>Submit issue</Button>
+      <SimpleMDE placeholder="Description" />
+      <Button>Submit</Button>
     </div>
   );
 };
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,16 @@
+import { Theme } from "@radix-ui/themes";
 import "@radix-ui/themes/styles.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import NavBar from "./NavBar";
-import { Theme } from "@radix-ui/themes";
 import "./globals.css";
+import "./theme-config.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({
+  subsets: ["latin"],
+  // display: "swap",
+  variable: "--font-inter",
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -18,9 +23,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
-        <Theme>
+    <html lang="en" className={inter.variable}>
+      <body>
+        <Theme accentColor="teal">
           <NavBar />
           <main className="p-8">{children}</main>
         </Theme>
