6b6e766ec0ff82847cd8ce6f2a08d5111aa1ed57
Rafael Cangahuala <78826890+rcpc265@users.noreply.github.com>
2024-01-22 21:55:24 -0500

    Show a confirmation dialog while trying to delete an issue

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { Spinner } from "@/components";
-import { Button } from "@radix-ui/themes";
+import { AlertDialog, Button, Flex } from "@radix-ui/themes";
 import axios from "axios";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
@@ -16,26 +16,92 @@ const DeleteIssueButton = ({ id, showText = true }: Props) => {
   const router = useRouter();
 
   return (
-    <Button
-      color="red"
-      disabled={isLoading}
-      onClick={async () => {
-        setIsLoading(true);
-        await axios.delete(`/api/issues/${id}`).catch((_err) => {
-          console.log("An error occurred while trying to delete the issue.");
-          setIsLoading(false);
-        });
-        router.push("/issues");
-        router.refresh();
-      }}
-    >
-      {isLoading ? (
-        <Spinner />
-      ) : (
-        <FiTrash2 className="stroke-[2px] text-[14px]" />
-      )}
-      {showText && <span className="font-medium">Delete Issue</span>}
-    </Button>
+    <AlertDialog.Root>
+      <AlertDialog.Trigger>
+        <Button color="red">
+          {isLoading ? (
+            <Spinner />
+          ) : (
+            <FiTrash2 className="stroke-[2px] text-[14px]" />
+          )}
+          {showText && <span className="font-medium">Delete Issue</span>}
+        </Button>
+      </AlertDialog.Trigger>
+      <AlertDialog.Content style={{ maxWidth: 450 }}>
+        <AlertDialog.Title>Delete Issue</AlertDialog.Title>
+        <AlertDialog.Description size="2">
+          Are you sure? This issue will be permanently deleted.
+        </AlertDialog.Description>
+
+        <Flex gap="3" mt="4" justify="end">
+          <AlertDialog.Cancel>
+            <Button variant="soft" color="gray">
+              Cancel
+            </Button>
+          </AlertDialog.Cancel>
+          <AlertDialog.Action>
+            <Button
+              variant="solid"
+              color="red"
+              onClick={async () => {
+                setIsLoading(true);
+                await axios.delete(`/api/issues/${id}`).catch((_err) => {
+                  console.log(
+                    "An error occurred while trying to delete the issue."
+                  );
+                  setIsLoading(false);
+                });
+                router.push("/issues");
+                router.refresh();
+              }}
+            >
+              Delete Issue
+            </Button>
+          </AlertDialog.Action>
+        </Flex>
+      </AlertDialog.Content>
+    </AlertDialog.Root>
   );
 };
+
+// "use client";
+// import { Spinner } from "@/components";
+// import { Button } from "@radix-ui/themes";
+// import axios from "axios";
+// import { useRouter } from "next/navigation";
+// import { useState } from "react";
+// import { FiTrash2 } from "react-icons/fi";
+
+// interface Props {
+//   id: number;
+//   showText?: boolean;
+// }
+
+// const DeleteIssueButton = ({ id, showText = true }: Props) => {
+//   const [isLoading, setIsLoading] = useState(false);
+//   const router = useRouter();
+
+//   return (
+//     <Button
+//       color="red"
+//       disabled={isLoading}
+//       onClick={async () => {
+//         setIsLoading(true);
+//         await axios.delete(`/api/issues/${id}`).catch((_err) => {
+//           console.log("An error occurred while trying to delete the issue.");
+//           setIsLoading(false);
+//         });
+//         router.push("/issues");
+//         router.refresh();
+//       }}
+//     >
+//       {isLoading ? (
+//         <Spinner />
+//       ) : (
+//         <FiTrash2 className="stroke-[2px] text-[14px]" />
+//       )}
+//       {showText && <span className="font-medium">Delete Issue</span>}
+//     </Button>
+//   );
+// };
 export default DeleteIssueButton;
--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,9 +1,10 @@
 import { IssueStatusBadge, Link } from "@/components";
 import prisma from "@/prisma/client";
-import { Flex, Table } from "@radix-ui/themes";
+import { Box, Button, Flex, Table, Text } from "@radix-ui/themes";
 import IssueActions from "./IssueActions";
-import EditIssueButton from "./[id]/EditIssueButton";
 import DeleteIssueButton from "./[id]/DeleteIssueButton";
+import EditIssueButton from "./[id]/EditIssueButton";
+import { FiPlusCircle } from "react-icons/fi";
 
 const IssuesPage = async () => {
   const issues = await prisma.issue.findMany({
@@ -13,46 +14,66 @@ const IssuesPage = async () => {
   return (
     <>
       <IssueActions />
-      <Table.Root variant="surface" mt="5">
-        <Table.Header>
-          <Table.Row>
-            <Table.ColumnHeaderCell>Issue</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell className="hidden md:table-cell">
-              Status
-            </Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell className="hidden md:table-cell">
-              Created
-            </Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell>Actions</Table.ColumnHeaderCell>
-          </Table.Row>
-        </Table.Header>
-        <Table.Body>
-          {issues.map((issue) => (
-            <Table.Row key={issue.id}>
-              <Table.Cell>
-                <Flex gap="2" align="center">
-                  <Link href={`/issues/${issue.id}`}>{issue.title}</Link>
-                  <span className="md:hidden">
-                    <IssueStatusBadge status={issue.status} />
-                  </span>
-                </Flex>
-              </Table.Cell>
-              <Table.Cell className="hidden md:table-cell">
-                <IssueStatusBadge status={issue.status} />
-              </Table.Cell>
-              <Table.Cell className="hidden md:table-cell">
-                {issue.createdAt.toDateString()}
-              </Table.Cell>
-              <Table.Cell>
-                <Flex gap="2">
-                  <EditIssueButton id={issue.id} showText={false} />
-                  <DeleteIssueButton id={issue.id} showText={false} />
-                </Flex>
-              </Table.Cell>
+      {issues.length === 0 ? (
+        <Flex
+          align="center"
+          justify="center"
+          direction="column"
+          gap="3"
+          className="h-[calc(100vh-10rem)]"
+        >
+          <Link href="/issues/new">
+            <FiPlusCircle className="text-black hover:scale-105 rounded-full transition-all text-[50px]" />
+          </Link>
+          <Text>No issues found. Start creating issues now!</Text>
+          <Button>
+            <Link href="/issues/new">
+              <span className="text-white">Create issue</span>
+            </Link>
+          </Button>
+        </Flex>
+      ) : (
+        <Table.Root variant="surface" mt="5">
+          <Table.Header>
+            <Table.Row>
+              <Table.ColumnHeaderCell>Issue</Table.ColumnHeaderCell>
+              <Table.ColumnHeaderCell className="hidden md:table-cell">
+                Status
+              </Table.ColumnHeaderCell>
+              <Table.ColumnHeaderCell className="hidden md:table-cell">
+                Created
+              </Table.ColumnHeaderCell>
+              <Table.ColumnHeaderCell>Actions</Table.ColumnHeaderCell>
             </Table.Row>
-          ))}
-        </Table.Body>
-      </Table.Root>
+          </Table.Header>
+          <Table.Body>
+            {issues.map((issue) => (
+              <Table.Row key={issue.id}>
+                <Table.Cell>
+                  <Flex gap="2" align="center">
+                    <Link href={`/issues/${issue.id}`}>{issue.title}</Link>
+                    <span className="md:hidden">
+                      <IssueStatusBadge status={issue.status} />
+                    </span>
+                  </Flex>
+                </Table.Cell>
+                <Table.Cell className="hidden md:table-cell">
+                  <IssueStatusBadge status={issue.status} />
+                </Table.Cell>
+                <Table.Cell className="hidden md:table-cell">
+                  {issue.createdAt.toDateString()}
+                </Table.Cell>
+                <Table.Cell>
+                  <Flex gap="2">
+                    <EditIssueButton id={issue.id} showText={false} />
+                    <DeleteIssueButton id={issue.id} showText={false} />
+                  </Flex>
+                </Table.Cell>
+              </Table.Row>
+            ))}
+          </Table.Body>
+        </Table.Root>
+      )}
     </>
   );
 };
