29c9c220168018aa49aa92a76b7ee75287b65ef8
Rafael Cangahuala <78826890+rcpc265@users.noreply.github.com>
2024-01-24 08:57:17 -0500

    Add user avatar dropdown

--- app/AuthLink.tsx
+++ app/AuthLink.tsx
@@ -1,10 +1,11 @@
 "use client";
 import { Spinner } from "@/components";
+import { Avatar, DropdownMenu, Text } from "@radix-ui/themes";
 import { signIn, signOut, useSession } from "next-auth/react";
 import { useState } from "react";
 
 const AuthLink = () => {
-  const { status } = useSession();
+  const { data: session, status } = useSession();
   const [isSigningIn, setIsSigningIn] = useState(false);
   const isLoading = status === "loading" || isSigningIn;
 
@@ -20,13 +21,25 @@ const AuthLink = () => {
   switch (status) {
     case "authenticated":
       return (
-        <button
-          onClick={() => {
-            signOut();
-          }}
-        >
-          Sign out
-        </button>
+        <DropdownMenu.Root>
+          <DropdownMenu.Trigger>
+            <Avatar
+              src={session.user!.image!}
+              fallback="?"
+              size="2"
+              radius="full"
+              className="cursor-pointer"
+            />
+          </DropdownMenu.Trigger>
+          <DropdownMenu.Content>
+            <DropdownMenu.Label>
+              <Text size="2">{session.user!.email}</Text>
+            </DropdownMenu.Label>
+            <DropdownMenu.Item onSelect={() => signOut()}>
+              Sign out
+            </DropdownMenu.Item>
+          </DropdownMenu.Content>
+        </DropdownMenu.Root>
       );
     case "unauthenticated":
       return <button onClick={handleSignIn}>Sign in</button>;
