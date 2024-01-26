abcc73c7df089f1113162d3fd2198f22a23f0831
Rafael Cangahuala <78826890+rcpc265@users.noreply.github.com>
2024-01-26 15:04:45 -0500

    Update issue details page

--- app/issues/[id]/loading.tsx
+++ app/issues/[id]/loading.tsx
@@ -1,25 +1,44 @@
+import authOptions from "@/app/api/auth/[...nextauth]/authOptions";
 import { Skeleton } from "@/components";
 import { Box, Card, Flex } from "@radix-ui/themes";
+import { getServerSession } from "next-auth";
+import IssueDetailsLayout from "../_components/IssueDetailsLayout";
+import AssigneeSelectSkeleton from "./AssigneeSelectSkeleton";
 
-const LoadingIssueDetailPage = () => {
+const LoadingIssueDetailPage = async () => {
+  const session = await getServerSession(authOptions);
+
   return (
-    <Box className="max-w-lg mx-auto">
-      <Flex gap="3" align="center">
-        {/* Issue title */}
-        <Skeleton height={22} width={220} />
-
-        {/* Issue status */}
-        <Skeleton width={45} />
+    <IssueDetailsLayout>
+      <Flex gap="5">
+        {/* Issue details */}
+        <Box className="w-full">
+          <Flex gap="3" align="center">
+            {/* Issue title */}
+            <Skeleton height={22} width={220} />
+            {/* Issue status */}
+            <Skeleton height={20} width={45} />
+          </Flex>
+          <Card className="prose" mt="5">
+            {/* Issue description */}
+            <Skeleton count={3} />
+          </Card>
+          <Box mt="2">
+            {/* Issue created at */}
+            <Skeleton width={150} />
+          </Box>
+        </Box>
+        {session && (
+          <Flex className="w-[200px]" direction="column" gap="3" mt="4">
+            <AssigneeSelectSkeleton widthFull={true} />
+            {/* Delete Issue Button */}
+            <Skeleton />
+            {/* Edit Issue Button */}
+            <Skeleton />
+          </Flex>
+        )}
       </Flex>
-      <Card className="prose" mt="5">
-        {/* Issue description */}
-        <Skeleton count={3} />
-      </Card>
-      <Box mt="2">
-        {/* Issue created at */}
-        <Skeleton width={150} />
-      </Box>
-    </Box>
+    </IssueDetailsLayout>
   );
 };
 export default LoadingIssueDetailPage;
--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,21 +1,37 @@
+import authOptions from "@/app/api/auth/[...nextauth]/authOptions";
 import prisma from "@/prisma/client";
-import { Box } from "@radix-ui/themes";
+import { Flex } from "@radix-ui/themes";
+import { getServerSession } from "next-auth";
 import { notFound } from "next/navigation";
+import AssigneeSelect from "./AssigneeSelect";
+import DeleteIssueButton from "./DeleteIssueButton";
+import EditIssueButton from "./EditIssueButton";
 import IssueDetails from "./IssueDetails";
+import IssueDetailsLayout from "../_components/IssueDetailsLayout";
 
 interface Props {
   params: { id: string };
 }
 
 const IssueDetailPage = async ({ params: { id } }: Props) => {
+  const session = await getServerSession(authOptions);
   const issue = await prisma.issue.findUnique({ where: { id: +id } });
 
   if (!issue) notFound();
 
   return (
-    <Box className="max-w-lg mx-auto">
-      <IssueDetails issue={issue} />
-    </Box>
+    <IssueDetailsLayout>
+      <Flex gap="5">
+        <IssueDetails issue={issue} />
+        {session && (
+          <Flex className="w-[200px]" direction="column" gap="3" mt="4">
+            <AssigneeSelect issue={issue} widthFull />
+            <DeleteIssueButton id={issue.id} />
+            <EditIssueButton id={issue.id} />
+          </Flex>
+        )}
+      </Flex>
+    </IssueDetailsLayout>
   );
 };
 export default IssueDetailPage;
